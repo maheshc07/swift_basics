b29cd1c938bce2626ae7b745ca058c66f5845752
maheshc07 <mjmahi90@gmail.com>
2023-04-08 15:39:26 +0100

    Continued adding closures code

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -710,3 +710,42 @@ let sayHello2 = { (name: String) -> String in
 
 sayHello2("Mahesh")
 
+func getUserData(for id1: Int) -> String {
+    if id1 == 1989 {
+        return "Taylor Swift"
+    } else {
+        return "Anonymous"
+    }
+}
+
+let data: (Int) -> String = getUserData
+let user3 = data(1989)
+print(user3)
+
+let team =  ["Gloria", "Suzanne", "Piper", "Tiffany", "Tasha"]
+let sortedTeam = team.sorted()
+print(team)
+print(sortedTeam)
+
+func captainFirstSorted(name1: String, name2: String)  -> Bool {
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+    return name1 < name2
+}
+
+let captainFirstTeam1 = team.sorted(by: captainFirstSorted)
+print(captainFirstTeam1)
+
+let captainFirstTeam = team.sorted(by: {(name1: String, name2: String)  -> Bool in
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+    return name1 < name2
+})
+
+print(captainFirstTeam)
